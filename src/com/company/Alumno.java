e30b1c94afb9428fff6a8c983c305b6d3b247c60
AriS <arielr.suarez@gmail.com>
2019-09-07 18:28:32 -0300

    Parte C - Creo Profesor con getters y setters

--- src/com/company/Alumno.java
+++ src/com/company/Alumno.java
@@ -5,11 +5,19 @@ public class Alumno {
     private String apellido;
     private Integer codigoDeAlumno;
 
+    /**
+     * Creamos el constructor para cuando se crea un alumno
+     * @param nombre
+     * @param apellido
+     * @param codigoDeAlumno
+     */
+
     public Alumno(String nombre, String apellido, Integer codigoDeAlumno) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.codigoDeAlumno = codigoDeAlumno;
     }
 
-    
+
+
 }
---
+++ src/com/company/Curso.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Curso {
+
+    private String nombre;
+    private  Integer curso;
+
+    /**
+     * Creo los getters y setters para el curso
+     * @return
+     */
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Integer curso) {
+        this.curso = curso;
+    }
+}
+
+
