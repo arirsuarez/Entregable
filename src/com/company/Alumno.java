c029370b8ba2e8d7565143347653caa7d77f2f9e
AriS <arielr.suarez@gmail.com>
2019-09-07 18:12:59 -0300

    Parte A - Creo Alumno

---
+++ src/com/company/Alumno.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private Integer codigoDeAlumno;
+
+    public Alumno(String nombre, String apellido, Integer codigoDeAlumno) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigoDeAlumno = codigoDeAlumno;
+    }
+
+    
+}
